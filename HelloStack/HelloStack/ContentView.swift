471029d7ddc0c785ffdd469ac748c44864c63f36
maekenn <98858722+maekenn@users.noreply.github.com>
2022-09-15 16:13:55 +0900

    secondcommit

--- HelloStack/HelloStack/ContentView.swift
+++ HelloStack/HelloStack/ContentView.swift
@@ -26,7 +26,6 @@ struct ContentView: View {
                 Rectangle()
                     .foregroundColor(.yellow)
                     .frame(width: 50, height: 50)
-                
             }
             ZStack{
                 Rectangle()
@@ -53,8 +52,125 @@ struct ContentView: View {
                 }
             }
         }
+        
+        //練習1
+        VStack {
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(width: 200, height: 50)
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.yellow)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.brown)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.green)
+                .frame(width: 180, height: 20)
+            Rectangle()
+                .foregroundColor(.orange)
+                .frame(width: 200, height: 50)
+        }
+        
+        //練習2
+        ZStack {
+            Rectangle()
+                .foregroundColor(.green)
+                .frame(width: 300, height: 300)
+            
+            VStack {
+                HStack {
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 70, height: 70)
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 70, height: 70)
+                }
+                VStack {
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 50, height: 20)
+                }
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 100, height: 80)
+            }
+            
+        }
+        
+        //練習3
+        VStack {
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 300, height: 300)
+                VStack {
+                    HStack{
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 60, height: 20)
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.gray)
+                                .frame(width: 90, height: 90)
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 70, height: 70)
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.gray)
+                                .frame(width: 90, height: 90)
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 70, height: 70)
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .frame(width: 20, height: 20)
+                        }
+                        
+                        
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 60, height: 20)
+                    }
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 70, height: 40)
+                        VStack {
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .frame(width: 60, height: 10)
+                            Rectangle()
+                                .foregroundColor(.red)
+                                .frame(width: 60, height: 10)
+                        }
+                    }
+                }
+            }
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(width: 300, height: 100)
+            HStack{
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+                Rectangle()
+                    .foregroundColor(.black)
+                    .frame(width: 70, height: 30)
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
