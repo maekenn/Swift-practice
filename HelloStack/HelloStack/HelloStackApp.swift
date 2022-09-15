afd745f6bf1bbcf20db017713a900f5f8ebb64a5
maekenn <98858722+maekenn@users.noreply.github.com>
2022-09-15 15:03:48 +0900

    firstcommit

---
+++ HelloStack/HelloStack/HelloStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloStackApp.swift
+//  HelloStack
+//
+//  Created by 前川健太 on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct HelloStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
