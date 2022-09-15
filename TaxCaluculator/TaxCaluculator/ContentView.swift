3af85bee10cbf4bb5c78307721b5be2581ccfaec
maekenn <98858722+maekenn@users.noreply.github.com>
2022-09-15 18:54:36 +0900

    add

---
+++ TaxCaluculator/TaxCaluculator/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  TaxCaluculator
+//
+//  Created by 前川健太 on 2022/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var inputText = ""
+    @State var tax8 = 0.0
+    @State var tax10 = 0.0
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            TextField("ここに文字を入力", text: $inputText)
+                .keyboardType(.numberPad)
+            
+            Button("計算") {
+                tax8 = (Double(inputText) ?? 0) * 0.08
+                tax10 = (Double(inputText) ?? 0) * 0.1
+            }
+            Text("価格: \(inputText)")
+            Text("消費税8％:\(tax8)")
+            Text("消費税10％:\(tax10)")
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ TaxCaluculator/TaxCaluculator/TaxCaluculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCaluculatorApp.swift
+//  TaxCaluculator
+//
+//  Created by 前川健太 on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct TaxCaluculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
